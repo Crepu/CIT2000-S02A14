cffa7c8e42ce4bffe3b12e4adcbba7d7e1d6bcdf
Crepu <danielmendez310@gmail.com>
2014-09-17 15:51:45 -0300

    Pauta Control 1

--- p1c1.cpp
+++ p1c1.cpp
@@ -42,15 +42,37 @@ class Fila
 	Grupo salida(int disp)
 	{
 		Grupo *aux = ini;
+		Grupo ant_aux;
 		while(aux != null)
 		{
 			if(aux->cantidad > disp)
 			{
+				ant_aux = aux;
 				aux = aux->sig;
 			}
 			else
 				break;
 		}
+
+		//El elegido esta al inicio
+		if(ini == aux)
+		{
+			ini = aux->sig;
+			aux->sig = null;
+			return aux;
+		}
+
+		//El elegido esta al final
+		if(fin == aux)
+		{
+			fin = ant_aux;
+			fin->sig = null;
+			return aux;
+		}
 		
+		//El elegido esta al medio 
+		ant_aux->sig = aux->sig;
+		aux->sig = null;
+		return aux;
 	}
 };
